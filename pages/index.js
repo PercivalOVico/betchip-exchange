83918ed00591eaffc8a4f133f4e16c90afac094e
Percival O Vico <percifico@gmail.com>
2022-09-02 14:31:14 +0300

    0.03

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Fragment } from 'react'
 import styles from '../styles/Home.module.css'
-import Guest from './guest/guest'
+import GuestLandingPage from './guest/guestLandingPage'
+
 
 export default function Home() {
   return (
     <Fragment >
-     <Guest />
+     <GuestLandingPage />
     </Fragment>
   )
 }
